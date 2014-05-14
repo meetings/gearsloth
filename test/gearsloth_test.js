46b42a89183c3ba0c558e2889ac578529b97924e
Lauri Kangassalo <lauri.kangassalo@helsinki.fi>
2014-05-14 14:44:54 +0300

    added a test for decodeTask

--- test/gearsloth_test.js
+++ test/gearsloth_test.js
@@ -2,21 +2,30 @@ var assert = require("assert");
 var gearsloth = require('../lib/gearsloth');
 describe('Array', function(){
   var at = 'a';
-  var func_name = 'a';
-  var payload = new Buffer('k', 'utf8');
+  var func_name = 'b';
+  var payload = new Buffer('c', 'utf8');
+  var testBuffer = new Buffer('a\0b\0c', 'utf8');
   setup(function(){
   });
   describe('#encodeTask()', function(){
     test('should return buffer with null byte delimiters', function(){
-      var testBuffer = new Buffer('a\0a\0k', 'utf8');
       assert.deepEqual(testBuffer, gearsloth.encodeTask(at, func_name, payload));
     });
   });    
   describe('#encodeTask()', function(){
     test('should not fail with payload containing null bytes', function(){
-      payload = new Buffer('k\0k', 'utf8');
-      var testBuffer = new Buffer('a\0a\0k\0k', 'utf8');
+      var payload = new Buffer('c\0d', 'utf8');
+      var testBuffer = new Buffer('a\0b\0c\0d', 'utf8');
       assert.deepEqual(testBuffer, gearsloth.encodeTask(at, func_name, payload));
+    });
+  });
+
+  describe('#decodeTask()', function(){
+    test('should return correct JSON object', function(){
+      var shouldBe = {at:at,
+        func_name:func_name,
+        payload:payload};
+      assert.deepEqual(shouldBe, gearsloth.decodeTask(testBuffer));
     });
   });
 });
