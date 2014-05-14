65f7334dac55491019bfe19fdabeb1857acb2749
Jarno Leppänen <jarno.leppanen@iki.fi>
2014-05-14 17:24:50 +0300

    deleted reverse worker/client

--- reverse-client.js
+++
@@ -1,9 +0,0 @@
-var Client = require('gearman-coffee').Client;
-
-var client = new Client();
-
-client.submitJob('reverse', 'kitteh').
-on('complete', function(handle, data) {
-  console.log(data.toString('utf-8'));
-  client.disconnect();
-});
--- reverse.js
+++
@@ -1,6 +0,0 @@
-var Worker = require('gearman-coffee').Worker;
-
-var worker = new Worker('reverse', function(payload, worker) {
-  var reversed = payload.toString("utf-8").split('').reverse().join('');
-  return worker.complete(reversed);
-});
