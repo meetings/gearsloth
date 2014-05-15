e2e5f49977dc88ff73194a5888a975ecfacbecc7
Lauri Kangassalo <lauri.kangassalo@helsinki.fi>
2014-05-15 18:11:38 +0300

    added tests that DO NOT WORK

---
+++ test/unit/sqlite-test.js
@@ -0,0 +1,57 @@
+var chai = require("chai");
+var adapter = require('../../lib/adapters/sqlite');
+var sqlite3 = require('sqlite3').verbose();
+
+var expect = chai.expect;
+
+describe('sqlite-adapter', function() {
+  var second_ago = new Date() - 1000;
+  var func_name = 'log';
+  var payload = new Buffer(10);
+  var test_json = {
+    at: second_ago,
+    func_name: func_name,
+    payload: payload
+  };
+  var testdb;
+  var conn;
+  setup(function() {
+    adapter.initializeWithHandle();
+  });
+  suite('saveTask() and readNextTasks()', function() {
+    test('should insert JSON task into database', function() {
+      adapter.saveTask(test_json);
+      adapter.readNextTasks(function(task) {
+        expect(task).to.have.property('id', 'at', 'func_name', 'payload');
+      });
+    });
+    test('should insert task parameters into database', function() {
+      adapter.saveTask(second_ago, func_name, payload);
+      adapter.readNextTasks(function(task) {
+        expect(task).to.have.property('id', 'at', 'func_name', 'payload');
+      });
+    });
+    test('should store multiple tasks', function() {
+      adapter.saveTask(test_json);
+      adapter.saveTask(test_json);
+      adapter.saveTask(test_json);
+      var tasks = [];
+      var count = 0;
+      adapter.readNextTasks(function(task) {
+        if(count)
+          expect(tasks).to.be(0);
+        tasks.push(task);
+        ++count;
+      });
+    });
+    test('should give unique ids to tasks', function() {
+      adapter.saveTask(test_json);
+      adapter.saveTask(test_json);
+      var id = [];
+      adapter.readNextTasks(function(task) {
+        expect(id).to.contain(task.id);
+        id.push(task.id);;
+      });
+    });
+  });
+});
