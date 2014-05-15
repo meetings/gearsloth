8d5b24d74c29692921a121fc17c75e38c3c3e7de
Frans Ojala <frans.ojala@cs.helsinki.fi>
2014-05-15 17:39:37 +0300

    geasrlothd.js now saves into sqlite database

--- gearslothd.js
+++ gearslothd.js
@@ -2,14 +2,14 @@ var gearman = require('gearman-coffee');
 var gearsloth = require('./lib/gearsloth');
 var Worker = gearman.Worker;
 var Client = gearman.Client;
-var database = require('./lib/adapters/sqlite.js');
+var database = require('./lib/adapters/sqlite');
 
 var client = new Client();
 
 var worker = new Worker('submitJobDelayed', function(payload, worker) {
   var task = gearsloth.decodeTask(payload);
-  var dbconn = database.initializeWithHandle("DelayedTasks.sqlite");
-  dbconn.saveTask(new Date(task.at), task.func_name, task.payload);
+  var dbconn = database.initalizeWithHandle("DelayedTasks.sqlite");
+  database.saveTask(task);
 
 
   var timeout = new Date(task.at) - new Date();
