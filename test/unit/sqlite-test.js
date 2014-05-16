6180d735e32a732bf6301d56c65cc22c2c851761
Lauri Kangassalo <lauri.kangassalo@helsinki.fi>
2014-05-16 12:29:56 +0300

    fixed tests

--- test/unit/sqlite-test.js
+++ test/unit/sqlite-test.js
@@ -1,11 +1,15 @@
 var chai = require("chai");
+var chai_as_promised = require("chai-as-promised");
 var adapter = require('../../lib/adapters/sqlite');
 var sqlite3 = require('sqlite3').verbose();
 
+chai.use(chai_as_promised);
 var expect = chai.expect;
-
 describe('sqlite-adapter', function() {
+
+  // datetime a second a go
   var second_ago = new Date() - 1000;
+
   var func_name = 'log';
   var payload = new Buffer(10);
   var test_json = {
@@ -16,41 +20,60 @@ describe('sqlite-adapter', function() {
   var testdb;
   var conn;
   setup(function() {
+    // initialize a new db to mem before each test
     adapter.initializeWithHandle();
   });
   suite('saveTask() and readNextTasks()', function() {
-    test('should insert JSON task into database', function() {
+    test('should insert JSON task into database', function(done) {
       adapter.saveTask(test_json);
       adapter.readNextTasks(function(task) {
-        expect(task).to.have.property('id', 'at', 'func_name', 'payload');
+        try {
+          expect(task).to.have.property('at');        
+          expect(task).to.have.property('func_name');        
+          expect(task).to.have.property('payload');        
+        } catch(err) {
+          return done(err);
+        }
+        done();
       });
     });
-    test('should insert task parameters into database', function() {
+    test('should insert task parameters into database', function(done) {
       adapter.saveTask(second_ago, func_name, payload);
       adapter.readNextTasks(function(task) {
-        expect(task).to.have.property('id', 'at', 'func_name', 'payload');
+        try {
+          expect(task).to.have.property('at');        
+          expect(task).to.have.property('func_name');        
+          expect(task).to.have.property('payload');        
+        } catch(err) {
+          return done(err);
+        }
+        done();
       });
     });
-    test('should store multiple tasks', function() {
+    test('should give unique ids to tasks', function(done) {
       adapter.saveTask(test_json);
       adapter.saveTask(test_json);
       adapter.saveTask(test_json);
-      var tasks = [];
-      var count = 0;
+      var countdown = 3;
       adapter.readNextTasks(function(task) {
-        if(count)
-          expect(tasks).to.be(0);
-        tasks.push(task);
-        ++count;
+        --countdown;
+        if(countdown === 0)
+          done();
       });
     });
-    test('should give unique ids to tasks', function() {
+    test('should give unique ids to tasks', function(done) {
       adapter.saveTask(test_json);
       adapter.saveTask(test_json);
-      var id = [];
+      var id = -1;
       adapter.readNextTasks(function(task) {
-        expect(id).to.contain(task.id);
-        id.push(task.id);;
+        try {
+          expect(id).to.not.equal(task.id);
+        } catch(err) {
+          return done(err);
+        }
+        if(id != -1)
+          done();
+        id = task.id;
       });
     });
   });
