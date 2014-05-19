0f61230440fbd4487492fa8543b66ce7f36c31f5
Frans Ojala <frans.ojala@cs.helsinki.fi>
2014-05-19 12:09:07 +0300

    adapter is going to change a lot

--- examples/sqlite-adapter-example.js
+++ examples/sqlite-adapter-example.js
@@ -2,16 +2,26 @@
 var db = require('../lib/adapters/sqlite');
 
 // initialize with a handle into a regular file, empty for in-memory
-var dbconn = db.initialize("DelayedTasks.sqlite");
+db.initialize("DelayedTasks.sqlite", afterInit );
+
+function afterInit(dbconn) {
+	dbconn.saveTask(new Date(), 'log', 'kittehs');
+
+	dbconn.readNextTasks(function (err, task) {
+  		console.log(task);
+	});
+
+	dbconn.close();
+}
 
 // check what is the status of the database connection
 console.log(dbconn);
 
 // save a task with current date
-db.saveTask(new Date(), 'log', 'kittehs');
+dbconn.saveTask(new Date(), 'log', 'kittehs');
 
 // read all expired tasks from db and print them
-db.readNextTasks(function (err, task) {
+dbconn.readNextTasks(function (err, task) {
   console.log(task);
 });
 
