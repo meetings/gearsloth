93e56542b97ef5122bf3fce33c27a99fedc25eeb
Tero Keinänen <kenkku@kenkku.net>
2014-05-21 18:14:25 +0300

    add sinon for mocking, test passthrough strategy by mocking

--- test/unit/passthrough-test.js
+++ test/unit/passthrough-test.js
@@ -1,12 +1,17 @@
 var chai = require("chai");
+var sinon = require('sinon');
+var gearman = require('gearman-coffee');
 var Passthrough = require("../../lib/strategies/passthrough");
 
-var expect = chai.expect;
+chai.should();
 
 describe('passthrough strategy', function() {
   suite('construction', function() {
-    test('can create Passthrough strategy', function() {
-      var p = new Passthrough();
-    });
+    test('can create Passthrough strategy', sinon.test(function() {
+      this.stub(gearman);
+      var p = new Passthrough(gearman);
+      gearman.Worker.calledWithNew().should.be.true;
+      gearman.Worker.calledOnce.should.be.true;
+    }));
   });
 });
