a53c272da7c1a7670cea725e4b1403c575f122f4
Tuomas Starck <dev@starck.fi>
2014-05-22 13:40:44 +0300

    whitespace fixes

--- examples/sqlite-adapter-example.js
+++ examples/sqlite-adapter-example.js
@@ -1,26 +1,25 @@
-// require the db adapter
 var db = require('../lib/adapters/sqlite');
 
 // initialize with a handle into a regular file, empty for in-memory
-db.initialize("DelayedTasks.sqlite", afterInit );
+db.initialize("DelayedTasks.sqlite", afterInit);
 
 function afterInit(err, dbconn) {
 
   if (err) console.log(err);
 
-	var stop = dbconn.listenTask(function (err, task_id) {
-	    if (err) console.log(err);
+  var stop = dbconn.listenTask(function (err, task_id) {
+      if (err) console.log(err);
       stop();
       dbconn.grabTask(task_id, function(task) {
         console.log(task);
       });
-	});
-	
-	var example_task = {
-	  at: new Date(),
-	  func_name: 'log',
-	  payload: 'kittehs'
-	}
+  });
 
-	dbconn.saveTask(example_task, function() {});
+  var example_task = {
+    at: new Date(),
+    func_name: 'log',
+    payload: 'kittehs'
+  };
+
+  dbconn.saveTask(example_task, function() {});
 }
