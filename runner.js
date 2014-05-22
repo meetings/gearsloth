b6d1d8ffd40e637a50f106f77f1a527c93164d5e
Samppalol <sami.salonen@yahoo.com>
2014-05-22 16:15:53 +0300

    worker and runner now make use of the logging interface

--- runner.js
+++ runner.js
@@ -1,17 +1,23 @@
 var gearman = require('gearman-coffee');
+var logger = require('./lib/log.js')
 var Client = gearman.Client;
 
 module.exports = function(config) {
   var client = new Client();
-  config.dbconn.listenTask(function(err, task) {
-    if (err) return console.error('error reading task');
-    var timeout = task.at - new Date();
-    if (timeout < 0)
-      timeout = 0;
-    setTimeout(function() {
-      client.submitJob(task.func_name, task.payload).
-      on('complete', function() {}); // do nothing
-    }, timeout);
-  });
-
+  try {
+    config.dbconn.listenTask(function(err, task) {
+      if (err) return logger.err(err);
+      var timeout = task.at - new Date();
+      if (timeout < 0)
+        timeout = 0;
+      setTimeout(function() {
+        client.submitJob(task.func_name, task.payload).
+        on('complete', function() {}); // do nothing
+      }, timeout);
+    });
+  }
+  catch(err) {
+    logger.err(err);
+  }
 };
+  
