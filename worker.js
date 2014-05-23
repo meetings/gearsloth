bc8b23a88dbd8120d15dfc36a51a0b916a42304c
Samppalol <sami.salonen@yahoo.com>
2014-05-23 13:43:02 +0300

    refactored logging for worker & runner

--- worker.js
+++ worker.js
@@ -7,24 +7,28 @@ module.exports = function(config) {
 
   // adapter backend
   function save(task, worker) {
-    try {
       config.dbconn.saveTask(task, function(err) {
         if (err) logger.err(err);
         return worker.complete();
       });
-    }
-    catch(err) {
-      logger.err(err);
-    };
   }
 
   // gearman interface
   var w_bin = new Worker('submitJobDelayed', function(payload, worker) {
-    var task = gearsloth.decodeTask(payload);
-    save(task, worker);
+    try {
+      var task = gearsloth.decodeTask(payload);    
+      save(task, worker);
+    } catch(err) {
+      logger.err(err);     
+    }
   });
+
   var w_json = new Worker('submitJobDelayedJson', function(payload, worker) {
-    var task = gearsloth.decodeJsonTask(payload);
-    save(task, worker);
+    try {
+      var task = gearsloth.decodeJsonTask(payload);  
+      save(task, worker);
+    } catch(err) {
+      logger.err(err);
+    }
   });
 };
