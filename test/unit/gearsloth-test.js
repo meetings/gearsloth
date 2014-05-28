70e5f953eb4cb87bc83376d708e3b80a6c08c387
Sami Salonen <sami.salonen@cs.helsinki.fi>
2014-05-28 15:51:25 +0300

    wrote unit tests for lib/gearsloth.encodeTask()

--- test/unit/gearsloth-test.js
+++ test/unit/gearsloth-test.js
@@ -31,12 +31,39 @@ suite('gearsloth', function() {
 
       [
         { func_name: new Buffer([0, 255]) },
-        '{"func_name_base64":"AP8="}']
+        '{"func_name_base64":"AP8="}'],
+      [
+        { func_name: 'foo', payload: new Buffer([0, 255]) },
+        '{"func_name":"foo","payload_base64":"AP8="}'
+      ],
+      [
+        { func_name: 'foo', payload: new Buffer([0, 255]), payload_base64: 'AP4=' },
+        '{"func_name":"foo","payload":[0,255],"payload_base64":"AP4="}'
+      ],
+      [
+        { func_name: 'foo', payload: 'bar', payload_base64: 'AP4=' },
+        '{"func_name":"foo","payload":"bar","payload_base64":"AP4="}'
+      ],
+      [
+        { func_name: new Buffer([0, 255]), func_name_base64: 'AP4=' },
+        '{"func_name":[0,255],"func_name_base64":"AP4="}'
+      ],
+      [
+        { func_name: 'bar', func_name_base64: 'AP4=' },
+        '{"func_name":"bar","func_name_base64":"AP4="}'
+      ]
 
     ].forEach(function(t) {
       testValidEncodeTask(t[0], t[1]);
     });
 
     // invalid tests
+    test('should throw without func_name field', function() {
+      expect(function() {
+        gearsloth.encodeTask({at: at, payload: 'lel'});
+      }).to.throw(Error);
+    })
   });
+
+  // TODO suite for decodeTask
 });
