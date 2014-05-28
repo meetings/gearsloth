19bc7a0258cfab9444524998404a1bb6bfe4cb52
Tero Keinänen <kenkku@kenkku.net>
2014-05-28 11:50:49 +0300

    ejector-e2e-test setup

--- test/e2e/ejector-e2e-test.js
+++ test/e2e/ejector-e2e-test.js
@@ -1,22 +1,46 @@
-var gearman = require('gearman-coffee');
-var child_process = require('child_process');
+var gearman = require('gearman-coffee')
+  , ejector = require('../../lib/daemon/ejector')
+  , sqlite = require('../../lib/adapters/sqlite')
+  , child_process = require('child_process')
+  , chai = require('chai')
+  , sinon = require('sinon')
+  , sinonChai = require('sinon-chai')
+  , parallel = require('async').parallel
+  , _ = require('underscore');
 
-suite('', function() {
+chai.should();
+chai.use(sinonChai);
 
-  var gearmand;
-  var client;
+suite('(e2e) ejector', function() {
+
+  var gearmand
+    , adapter
+    , client;
 
   setup(function(done) {
     gearmand = child_process.spawn('gearmand');
-    client = new gearman.Client().on('connect', done); // make sure gearmand is running
+    client = new gearman.Client();
+
+    parallel({
+      adapter: _.partial(sqlite.initialize, null),
+      client: _.bind(client.on, client, 'connect')
+    }, function(err, results) {
+      if(err) {
+        gearmand.kill();
+      }
+      adapter = results.adapter;
+      done(err);
+    });
   });
 
   teardown(function() {
     gearmand.kill();
   })
 
-  suite('', function() {
-    test('', function() {
-    });
+  test('should delete task from database', function() {
+    var conf = { dbconn: adapter };
+    //var completeSpy = sinon.spy(init.adapter, 'completeTask');
+
+
   });
 });
