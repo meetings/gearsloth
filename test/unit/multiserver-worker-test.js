d29f7e6efbe23b09b53d5d19b284afc938860532
Lauri Kangassalo <lauri.kangassalo@helsinki.fi>
2014-05-30 10:54:12 +0300

    refactored constructor to match that of gearman-coffee's

--- test/unit/multiserver-worker-test.js
+++ test/unit/multiserver-worker-test.js
@@ -46,6 +46,7 @@ suite("multiserver-worker", function() {
   suite("when given no servers", function() {
     setup(function() {
       sandbox.spy(exports, 'Worker');
+      m = new MultiserverWorker(
         null,
         'sample',
         dummy_func,
