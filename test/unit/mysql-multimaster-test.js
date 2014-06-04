88bade7fc922f20c471c3d0918885444b769e9d0
Tero Keinänen <kenkku@kenkku.net>
2014-06-04 09:59:13 +0300

    saveTask for mysql-multimaster

--- test/unit/mysql-multimaster-test.js
+++ test/unit/mysql-multimaster-test.js
@@ -2,7 +2,12 @@ var mysql = require('mysql')
   , MySQLMultimaster = require('../../lib/adapters/mysql-multimaster')
   , chai = require('chai')
   , expect = chai.expect
-  , sinon = require('sinon');
+  , sinon = require('sinon')
+  , any = sinon.match.any
+  , sinonchai = require('sinon-chai');
+
+chai.should();
+chai.use(sinonchai);
 
 suite('MySQL Multimaster adapter', function() {
   
@@ -70,5 +75,88 @@ suite('MySQL Multimaster adapter', function() {
     });
   });
 
+  suite('saveTask', function() {
+
+    var mysql_conn, adapter;
+
+    setup(function() {
+      mysql_conn = {
+        query: sandbox.stub().callsArgWith(2, null, 666)
+      };
+
+      mysql.createConnection.returns(mysql_conn);
+      adapter = new MySQLMultimaster.MySQLMultimaster(config);
+    });
+
+    test('with { func_name: "ebin" } should call query correctly', function(done) {
+      var task = {
+        func_name: 'ebin'
+      };
+      var task_to_insert = {
+        at: new Date(),
+        task: JSON.stringify(task)
+      };
+
+      adapter.saveTask(task, function(err, id) {
+        mysql_conn.query
+          .should.have.been.calledWith(any, task_to_insert);
+        done();
+      });
+    });
+
+    test('with { func_name: "ebin", at: "2014-06-07" } should call query correctly', function(done) {
+      var task = {
+        func_name: 'ebin',
+        at: new Date('2014-06-07')
+      };
+      var task_to_insert = {
+        at: new Date('2014-06-07'),
+        task: JSON.stringify(task)
+      };
+
+      adapter.saveTask(task, function(err, id) {
+        mysql_conn.query
+          .should.have.been.calledWith(any, task_to_insert);
+        done();
+      });
+    });
+
+    test('with { func_name: "ebin", after: 10 } should call query correctly', function(done) {
+      var task = {
+        func_name: 'ebin',
+        after: 10
+      };
+      var after = new Date( new Date().getTime() + 10000 );
+      var task_to_insert = {
+        at: after,
+        task: JSON.stringify(task)
+      };
+
+      adapter.saveTask(task, function(err, id) {
+        mysql_conn.query
+          .should.have.been.calledWith(any, task_to_insert);
+        done();
+      });
+    });
+
+    test('with { func_name: "ebin", after: "100" } should call query correctly', function(done) {
+      var task = {
+        func_name: 'ebin',
+        after: "100"
+      };
+      var after = new Date( new Date().getTime() + 100000 );
+      var task_to_insert = {
+        at: after,
+        task: JSON.stringify(task)
+      };
+
+      adapter.saveTask(task, function(err, id) {
+        mysql_conn.query
+          .should.have.been.calledWith(any, task_to_insert);
+        done();
+      });
+    });
+
+  });
 
 });
