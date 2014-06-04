224033fe3f96a82a39c82c02c01e2f19c619a9e4
Jarno Leppänen <jarno.leppanen@iki.fi>
2014-06-04 11:00:40 +0300

    Add system black box tests

---
+++ test/lib/spawn.js
@@ -0,0 +1,68 @@
+var child_process = require('child_process');
+var net = require('net');
+var async = require('async');
+
+var gearmand = exports.gearmand = function(port, done) {
+  var gearmand = child_process.spawn('gearmand',
+    ['-l', '/dev/null', '-p', port.toString()]);
+  connectUntilSuccess(port, done);
+  return gearmand;
+}
+
+var gearslothd = exports.gearslothd = function(port, done) {
+  var gearslothd = child_process.spawn('./bin/gearslothd',
+    ['--db=sqlite', '-v', 'localhost:' + port]);
+  readUntilMatch(gearslothd.stdout, /gearslothd: fully connected/, done);
+  return gearslothd;
+}
+
+var killall = exports.killall = function(processes, done) {
+  async.parallel(
+    processes.map(function(process) {
+      return function(callback) {
+        process.on('exit', function() {
+          // ignore exit code & signal
+          callback();
+        });
+      };
+    }),
+    function(err) {
+      done(err ? 'error killing processes' : null);
+    }
+  );
+  processes.forEach(function(process) {
+    process.kill();
+  });
+}
+
+// private
+
+function connectUntilSuccess(port, done) {
+  var socket = net.connect({
+    host: 'localhost',
+    port: port
+  }, function() {
+    socket.end();
+  })
+  .on('error', function(err) {
+    // catch error
+  })
+  .on('close', function(had_err) {
+    if (had_err)
+      connectUntilSuccess(port, done);
+    else
+      done();
+  });
+}
+
+function readUntilMatch(stream, regex, done) {
+  // stream is assumed to be line buffered
+  stream.setEncoding('utf8');
+  stream.on('data', function(line) {
+    if (line.match(regex))
+      done();
+  });
+  stream.on('end', function() {
+    // ignore
+  });
+}
