40b4fc67470729ceb7a22766f126839a0a2cd78a
Jarno Leppänen <jarno.leppanen@iki.fi>
2014-06-06 13:13:00 +0300

    Revert some non-working changes

--- .provision/apt.sh
+++ .provision/apt.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo 'Acquire::Languages "none";' > /etc/apt/apt.conf.d/99translations
 apt-get update
-apt-get install --yes --no-install-recommends gearman npm nodejs nodejs-legacy sqlite3
+apt-get install --yes --no-install-recommends gearman npm nodejs-legacy sqlite \
+  git
