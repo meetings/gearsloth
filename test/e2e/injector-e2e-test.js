d8d6bb136de4a1addc671330b0d2050e007a39d6
Frans Ojala <frans.ojala@cs.helsinki.fi>
2014-06-10 08:14:19 +0300

    injector tests, some fixes to multiserver client and worker, also runner and its tests

--- test/e2e/injector-e2e-test.js
+++ test/e2e/injector-e2e-test.js
@@ -85,16 +85,16 @@ suite('(e2e) injector', function() {
     teardown(function(done) {
       async.series([
         function (callback) {
-          client.disconnect();
           client.on('disconnect', function() {
             callback();
           });
-          callback();
+          client.disconnect();
         },
         function (callback) {
-          injector_in_use.disconnect(0, function(){
+          injector_in_use.on('disconnect', function() {
             callback();
           });
+          injector_in_use.disconnect(0, function(){});
         },
         function (callback) {
           spawn.killall([gearmand], callback);
@@ -196,16 +196,14 @@ suite('using a stubbed adapter that "fails",', function(){
   teardown(function(done) {
     async.series([
       function (callback) {
-        client.disconnect();
         client.on('disconnect', function() {
           callback();
         });
-        callback();
+        client.disconnect();
       },
       function (callback) {
-        injector_in_use.disconnect(0, function(){
-          callback();
-        });
+        injector_in_use.disconnect(0, function(){});
+        callback();
       },
       function (callback) {
         spawn.killall([gearmand], callback);
@@ -239,57 +237,5 @@ suite('using a stubbed adapter that "fails",', function(){
     });
   });
 });
-
-    // test('should disable task when runner_retry_count reaches 0', function(done) {
-    //   worker = new gearman.Worker('test', function(payload, worker) {
-    //     var json = JSON.parse(payload.toString());
-    //     adapter.disableTask.should.have.been.calledWith(json);
-    //     done();
-    //   }, { port:port 
-    //   });
-    //   adapter.listenTask = sinon.stub().yields(null, expiring_task1);
-    //   adapter.updateTask = sinon.stub().yields(null, 1);
-    //   adapter.disableTask = sinon.stub().yields(null, 1);
-    //   runner_in_use = runner(conf);
-    // });
-
-    // test('should not disable task when runner_retry_count has time to live', function(done) {
-    //   worker = new gearman.Worker('test', function(payload, worker) {
-    //     var json = JSON.parse(payload.toString());
-    //     adapter.disableTask.should.not.have.been.calledWith(json);
-    //     done();
-    //   }, { port:port 
-    //   });
-
-    //   adapter.listenTask = sinon.stub().yields(null, non_expiring_task1);
-    //   adapter.updateTask = sinon.stub().yields(null, 1);
-    //   adapter.disableTask = sinon.stub().yields(null, 1);
-    //   runner_in_use = runner(conf);
-    // });
-
-    // test('should call updateTask when a task is recieved', function(done) {
-    //   worker = new gearman.Worker('test', function(payload, worker) {
-    //     var json = JSON.parse(payload.toString());
-    //     adapter.updateTask.should.have.been.calledWith(json);
-    //     done();
-    //   }, { port:port
-    //   });
-    //   adapter.listenTask = sinon.stub().yields(null, sample_task1);
-    //   adapter.updateTask = sinon.stub().yields(null, 1);
-    //   runner_in_use = runner(conf);
-    // });
-
-    // test('should fetch a task from db and pass it on', function(done) {
-    //   worker = new gearman.Worker('test', function(payload, worker) {
-    //     var json = JSON.parse(payload.toString());
-    //     expect(json).to.have.property('id', sample_task1.id);
-    //     expect(json).to.have.property('func_name', sample_task1.func_name);
-
-    //     done();
-    //   }, {port:port});
-    //   adapter.listenTask = sinon.stub().callsArgWith(0, null, sample_task1);
-    //   adapter.updateTask = sinon.stub().callsArgWith(1, null);
-    //   runner = runner(conf);
-    // });
 
 });
