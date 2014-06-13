88b7b55bc07308256aa137281d006ca5dc765676
Jarno Leppänen <jarno.leppanen@iki.fi>
2014-06-13 17:45:34 +0300

    Fix controller function name

--- test/e2e/system-e2e-test.js
+++ test/e2e/system-e2e-test.js
@@ -18,11 +18,11 @@ suite('single gearslothd in default mode:', function() {
     verbose: 1,
     db: 'sqlite',
     dbopt: {
-      poll_timeout:0,
-      db_name:':memory:'
+      poll_timeout: 0,
+      db_name: ':memory:'
     },
     servers: [{
-      host:'localhost',
+      host: 'localhost',
       port: port
     }]
   };
