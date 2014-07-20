c1099149d9dfeddad8b6a714a64cf276e143b394
Antti Vähäkotamäki <antti@dicole.com>
2014-07-20 23:36:21 +0300

    first system smoke test

--- test/system/smoke-oneprocess-at.js
+++ test/system/smoke-oneprocess-at.js
@@ -1,223 +1,90 @@
+var lib = require( '../../lib/helpers/lib_require' );
+
 var async = require('async');
-var chai = require('chai');
-var expect = chai.expect;
-var Ejector = require('../../lib/daemon/ejector').Ejector;
-var Runner = require('../../lib/daemon/runner').Runner;
-var Injector = require('../../lib/daemon/injector').Injector;
-var gearman = require('gearman-coffee');
-var spawn = require('../lib/spawn');
-var child_process = require('child_process');
-var sqlite = require('../../lib/adapters/sqlite');
+var _ = require('underscore');
+var spawn = lib.require('test-helpers/spawn');
+
+var adapter_helper = lib.require('test-helpers/adapter-helper');
+var client_helper = lib.require('test-helpers/client-helper');
+var worker_helper = lib.require('test-helpers/worker-helper');
+var component_helper = lib.require('test-helpers/component-helper');
+
 var sinon = require('sinon');
 var sinonChai = require('sinon-chai');
-var Retry = require('../../lib/controllers/retry').Retry;
+var chai = require('chai');
+var expect = chai.expect;
 
 chai.should();
 chai.use(sinonChai);
 
-suite('blackbox: at on-time with sqlite :memory: "at" parameter', function() {
+suite('blackbox: at on-time with "at" parameter', function() {
 
   this.timeout(5000);
 
   var port = 54730;
-  var gearmand;
-  var injector;
-  var ejector;
-  var runner;
-  var client;
-  var worker;
-  var controller;
 
-  var port;
   var conf = {
-    dbopt: {
-      poll_timeout:50,
-      db_name:':memory:'
-    },
-    servers: [{
-      host:'localhost',
-      port: port
-    }]
-  };
-
-  var at_2000_task = {
-    func_name:'test',
-    payload:'blackbox-6000-ms-delay',
-    // at:time_in_future will be set in the test to circumvent some weird shit
-  };
-  var after_and_at_task = {
-    func_name:'test',
-    payload:'blackbox-after-supersedes',
-    at:new Date(),
-    after:6
+    db : adapter_helper.return_current_adapter_module(),
+    dbopt: adapter_helper.return_normal_dbopt(),
+    servers: [ { host : 'localhost', port : port } ]
   };
 
   setup(function(done) {
     async.series([
-      function(callback) {
-        sqlite.initialize(conf, function(err, dbconn) {
-            if (err) {
-              console.log(err, dbconn);
-              done("Error initializing database");
-              return;
-            }
-            conf.dbconn = dbconn;
-            sinon.spy(conf.dbconn, 'disableTask');
-            sinon.spy(conf.dbconn, 'updateTask');
-            sinon.spy(conf.dbconn, 'completeTask');
-            sinon.spy(conf.dbconn, 'saveTask');
-            sinon.spy(conf.dbconn, 'listenTask');
-            callback();
-        });
-      },
-      function(callback) {
-        gearmand = spawn.gearmand(port, function(){
-          callback();
-        });
-      },
-      function(callback) {
-        runner = new Runner(conf);
-        runner.on('connect', function(){
-          callback();
-        });
-      },
-      function(callback) {
-        injector = new Injector(conf);
-        injector.on('connect', function() {
-          callback();
-        });
-      },
-      function(callback) {
-        ejector = new Ejector(conf);
-        ejector.on('connect', function() {
-          callback();
-        });
-      },
-      function(callback) {
-        controller = new Retry(conf);
-        controller.on('connect', function(){
-          callback();
-        });
-      }
-      ], function() {
-        done();
-      })
+      adapter_helper.async_teardown( conf ),
+      spawn.async_gearmand( port ),
+      component_helper.async_setup_component_and_wait_for_connect( 'injector', conf ),
+      component_helper.async_setup_component_and_wait_for_connect( 'runner', conf ),
+      component_helper.async_setup_component_and_wait_for_connect( 'controller', conf ),
+      component_helper.async_setup_component_and_wait_for_connect( 'ejector', conf ),
+    ], done );
   });
 
   teardown(function(done) {
     async.series([
-      function(callback) {
-        runner.on('disconnect', function() {
-          callback();
-        })
-        runner.disconnect();
-      },
-      function(callback) {
-        injector.on('disconnect', function() {
-          callback();
-        });
-        injector.disconnect();
-      },
-      function(callback) {
-        ejector.on('disconnect', function() {
-          callback();
-        });
-        ejector.disconnect();
-      },
-      function(callback) {
-        controller.on('disconnect', function() {
-          callback();
-        });
-        controller.disconnect();
-      },
-      function(callback) {
-        client.on('disconnect', function() {
-          callback();
-        });
-        client.disconnect();
-      },
-      function(callback) {
-        if (worker) {
-          worker.on('disconnect', function() {
-            callback();
-          });
-          worker.disconnect();
-        } else {
-          callback();
-        }
-      },
-      function(callback) {
-        spawn.killall([gearmand], function() {
-          callback();
-        });
-      }
-      ], function() {
-        done();
-      });
+      component_helper.teardown,
+      worker_helper.teardown,
+      client_helper.teardown,
+      spawn.teardown,
+      adapter_helper.async_teardown( conf ),
+    ], done );
   });
 
   test('task is recieved in bottom level worker after timeout expires', function(done){
-    var time_in_future = new Date();
-    time_in_future.setSeconds(time_in_future.getSeconds() + 6 );
+    var at = new Date( new Date().getTime() + 2000 );
+    var task = {
+      func_name: 'test',
+      payload: '{ "ok" : "1" }',
+      at : at.toISOString(),
+    };
 
-    this.timeout(10000);
-    client = new gearman.Client({port:port});
-    var worker_fail = new gearman.Worker('test', function(payload, worker) {
-      done(new Error('task arrived too quickly'));
-    }, {port:port});
-    worker_fail.on('connect', function(){
-        at_2000_task.at = time_in_future;
-        client.submitJob('submitJobDelayed', JSON.stringify(at_2000_task));
-    });
+    worker_helper.register_worker_to_port_with_json_payload( 'test', port, function( data, worker ) {
+      worker.complete();
+      expect( data ).to.have.property( 'ok', "1" );
+      expect( new Date().getTime() ).to.be.within( new Date(at).getTime(), new Date(at).getTime() + 2000 );
+      done();
+    } );
 
-    setTimeout(function() {
-      async.series([
-        function(callback) {
-          worker_fail.socket.on('close', function(){
-            callback();
-          });
-          worker_fail.disconnect();
-        },
-        function(callback) {
-          worker = new gearman.Worker('test', function(payload, worker) {
-            var payload = payload.toString();
-            worker.complete();
-            expect(payload).to.equal(at_2000_task.payload);
-            done();
-            callback();
-          }, {port:port});
-        }]);
-    }, 5000);
+    client_helper.submit_delayed_job_to_port_and_return_job( task, port );
   });
 
   test('task is scheduled according to after if after and at are both specified', function(done){
-    this.timeout(10000);
-    client = new gearman.Client({port:port});
-    var worker_2_fail = new gearman.Worker('test', function(payload, worker) {  
-      done(new Error('task arrived too quickly'));
-    }, {port:port});
-    worker_2_fail.on('connect', function(){
-        client.submitJob('submitJobDelayed', JSON.stringify(after_and_at_task));
-    });
+    var start = new Date().getTime();
+    var at = new Date( start + 4000 );
+    var task = {
+      func_name: 'test',
+      payload: '{ "ok" : "2" }',
+      at : at.toISOString(),
+      after : 1
+    };
 
-    setTimeout(function() {
-      async.series([
-        function(callback) {
-          worker_2_fail.socket.on('close', function(){  
-            callback();
-          });
-          worker_2_fail.disconnect();
-        },
-        function(callback) {
-          worker = new gearman.Worker('test', function(payload, worker) {
-            var payload = payload.toString();
-            worker.complete();
-            expect(payload).to.equal(after_and_at_task.payload);
-            done();
-            callback();
-          }, {port:port});
-        }]);
-    }, 5000);
+    worker_helper.register_worker_to_port_with_json_payload( 'test', port, function( data ) {
+      expect( data ).to.have.property( 'ok', "2" );
+      expect( new Date().getTime() ).to.be.within( start + 1000, start + 3000 );
+      done();
+    } );
+
+    client_helper.submit_delayed_job_to_port_and_return_job( task, port )
+    .on('warning', function(err, err2, err3) { console.log( 1, err, err2, err3 )});
   });
-
 });
